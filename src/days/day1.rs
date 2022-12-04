ee2ca0eae348c6f9dd1f12cdd6e08aa8dfccddcc
Sam Morrell <samuelmorrell@gmail.com>
2022-12-04 16:24:59 +0000

    Day 1 working solution.
    It's not pretty, but it works!

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,6 +1,7 @@
 use crate::err::Error;
 use std::{
     path::Path,
+    io::BufReader
 };
 
 #[derive(Debug, Clone)]
@@ -23,8 +24,60 @@ impl ElfCalories {
 #[cfg(test)]
 mod tests {
 
+    use std::{
+        fs::File, 
+        io::{BufReader, BufRead}, vec,
+    };
+
     #[test]
     fn day1_part1() {
+        let file = File::open("data/day1/part1.txt").unwrap();
+        let lines: Vec<String> = BufReader::new(file).lines().into_iter().map(|l| l.unwrap()).collect();
+        let mut elf_calories: Vec<Vec<i32>> = vec![];
+        let mut curr_elf_cals: Vec<i32> = vec![];
+
+        for line in lines {
+            match line.as_str() {
+                "" => {
+                    elf_calories.push(curr_elf_cals);
+                    curr_elf_cals = vec![];
+                },
+                _ => {
+                    let val: i32 =  line.parse().unwrap();
+                    curr_elf_cals.push(val);
+                },
+            }
+        }
         
+        // Now I search for the largest value and print it. 
+        let maxval: i32 = elf_calories.iter().map(|cals| cals.iter().sum()).max().unwrap();
+        print!("{}", maxval);
     }
+
+    #[test]
+    fn day1_part2() {
+        let file = File::open("data/day1/part1.txt").unwrap();
+        let lines: Vec<String> = BufReader::new(file).lines().into_iter().map(|l| l.unwrap()).collect();
+        let mut elf_calories: Vec<Vec<i32>> = vec![];
+        let mut curr_elf_cals: Vec<i32> = vec![];
+
+        for line in lines {
+            match line.as_str() {
+                "" => {
+                    elf_calories.push(curr_elf_cals);
+                    curr_elf_cals = vec![];
+                },
+                _ => {
+                    let val: i32 =  line.parse().unwrap();
+                    curr_elf_cals.push(val);
+                },
+            }
+        }
+        
+        // Now I search for the largest value and print it. 
+        let mut sums: Vec<i32> = elf_calories.iter().map(|cals| cals.iter().sum()).collect();
+        sums.sort();
+        let top3: i32 = sums.iter().rev().take(3).sum();
+        println!("{}", top3);
+    } 
 }
