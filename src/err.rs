ca525288e92e05f765c505263957b56dc106e448
Sam Morrell <samuelmorrell@gmail.com>
2022-12-09 00:03:19 +0000

    Commit of both parts of day 5.
    This includes the implementation of required library code, as well as solutions for the example and both parts.

--- src/err.rs
+++ src/err.rs
@@ -1,10 +1,24 @@
 #[derive(Debug)]
 pub enum Error {
     IOError(std::io::Error),
+    RegexError(regex::Error),
+    ParseIntError(std::num::ParseIntError),
 }
 
 impl From<std::io::Error> for Error {
     fn from(err: std::io::Error) -> Self {
         Error::IOError(err)
+    }
+}
+
+impl From<regex::Error> for Error {
+    fn from(err: regex::Error) -> Self {
+        Error::RegexError(err)
+    }
+}
+
+impl From<std::num::ParseIntError> for Error {
+    fn from(err: std::num::ParseIntError) -> Self {
+        Error::ParseIntError(err)
     }
 }
