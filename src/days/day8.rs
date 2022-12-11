65cd33bb28442f458172419519fa1fb632b31188
Sam Morrell <samuelmorrell@gmail.com>
2022-12-11 22:18:10 +0000

    Finally came up with a working solution for d8 pt2
    This one took a little thinking, but I was able to use most of the existing code with a few additions to solve part 2.

--- src/days/day8.rs
+++ src/days/day8.rs
@@ -26,8 +26,17 @@ impl TreeMap {
     }
 
     pub fn on_boundary(&self, i: usize, j: usize) -> bool {
-        if i == 0 || j == 0 || i == self.heights.len() - 1|| j == self.heights.first().expect("Unable to find first row. ").len() - 1 {
-             true
+        if i == 0
+            || j == 0
+            || i == self.heights.len() - 1
+            || j == self
+                .heights
+                .first()
+                .expect("Unable to find first row. ")
+                .len()
+                - 1
+        {
+            true
         } else {
             false
         }
@@ -39,11 +48,6 @@ impl TreeMap {
         let tree = self.heights[i][j];
         let row = self.heights[i].clone();
         let col: Vec<usize> = self.heights.iter().map(|row| row[j].clone()).collect();
-        
-        let left: Vec<usize> = row[0..j].into_iter().map(|v| *v).collect();
-        let right: Vec<usize> = row[j+1..].into_iter().map(|v| *v).collect();
-        let up: Vec<usize> = col[0..i].into_iter().map(|v| *v).collect();
-        let down: Vec<usize> = col[i+1..].into_iter().map(|v| *v).collect();
 
         if self.on_boundary(i, j)
             || tree > row[0..j].iter().fold(usize::MIN, |a, &b| a.max(b))
@@ -60,16 +64,55 @@ impl TreeMap {
     pub fn count_visible(&self) -> usize {
         let mut total = 0;
         for i in 0..self.heights.len() {
-            for j in 0..self.heights.first().expect("Unable to find first row. ").len() {
+            for j in 0..self
+                .heights
+                .first()
+                .expect("Unable to find first row. ")
+                .len()
+            {
                 total += if self.is_visible(i, j) { 1 } else { 0 };
             }
         }
         total
     }
+
+    pub fn scenic_score(&self, i: usize, j: usize) -> usize {
+        let tree = self.heights[i][j];
+        let row = self.heights[i].clone();
+        let col: Vec<usize> = self.heights.iter().map(|row| row[j].clone()).collect();
+
+        let left: Vec<usize> = row[0..j].into_iter().map(|v| *v).collect();
+        let right: Vec<usize> = row[j + 1..].into_iter().map(|v| *v).collect();
+        let up: Vec<usize> = col[0..i].into_iter().map(|v| *v).collect();
+        let down: Vec<usize> = col[i + 1..].into_iter().map(|v| *v).collect();
+
+        let left_dist = left
+            .iter()
+            .rev()
+            .position(|t| *t >= tree)
+            .map_or(left.len(), |val| val + 1);
+        let right_dist = right
+            .iter()
+            .position(|t| *t >= tree)
+            .map_or(right.len(), |val| val + 1);
+        let up_dist = up
+            .iter()
+            .rev()
+            .position(|t| *t >= tree)
+            .map_or(up.len(), |val| val + 1);
+        let down_dist = down
+            .iter()
+            .position(|t| *t >= tree)
+            .map_or(down.len(), |val| val + 1);
+
+        left_dist * right_dist * down_dist * up_dist
+    }
 }
 
 #[cfg(test)]
 pub mod tests {
+    use itertools::Itertools;
+
     use super::TreeMap;
     use crate::io;
     use std::path::Path;
@@ -92,5 +135,49 @@ pub mod tests {
         let total_visible = map.count_visible();
 
         assert_eq!(total_visible, 1812)
+    }
+
+    #[test]
+    fn day8_part2_example() {
+        let input = io::read_string_col(Path::new("data/day8/example.txt"))
+            .expect("Unable to find input file. ");
+        let map = TreeMap::from_string_col(&input).expect("Unable to create tree map from input. ");
+
+        let scores: Vec<usize> = (0..map.heights.len())
+            .map(|i| {
+                (0..map
+                    .heights
+                    .first()
+                    .expect("Unable to find first row. ")
+                    .len())
+                    .map(|j| map.scenic_score(i, j))
+                    .collect::<Vec<usize>>()
+            })
+            .concat();
+
+        let max_score = scores.iter().fold(usize::MIN, |a, &b| a.max(b));
+        assert_eq!(max_score, 8);
+    }
+
+    #[test]
+    fn day8_part2() {
+        let input = io::read_string_col(Path::new("data/day8/data.txt"))
+            .expect("Unable to find input file. ");
+        let map = TreeMap::from_string_col(&input).expect("Unable to create tree map from input. ");
+
+        let scores: Vec<usize> = (0..map.heights.len())
+            .map(|i| {
+                (0..map
+                    .heights
+                    .first()
+                    .expect("Unable to find first row. ")
+                    .len())
+                    .map(|j| map.scenic_score(i, j))
+                    .collect::<Vec<usize>>()
+            })
+            .concat();
+
+        let max_score = scores.iter().fold(usize::MIN, |a, &b| a.max(b));
+        assert_eq!(max_score, 315495);
     }
 }
