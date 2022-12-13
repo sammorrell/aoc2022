d0c4009776a594161ae9fb1a06ab0ed47612e224
Sam Morrell <samuelmorrell@gmail.com>
2022-12-13 00:34:49 +0000

    Solution to part 1 of day 10.
    This is a simple solution, but it does seem to have worked. Onto part 2.

---
+++ src/days/day10.rs
@@ -0,0 +1,94 @@
+use std::clone;
+
+
+#[derive(Debug, Clone)]
+pub enum Command {
+    Noop,
+    Addx(i32),
+}
+
+impl Command {
+    pub fn from_str(input: &str) -> Command {
+        let segments: Vec<&str> = input.split(" ").collect();
+        match segments.first().expect("Empty string for input command. ") {
+            &"noop" => Self::Noop,
+            &"addx" => {
+                let operand = segments[1].parse::<i32>().expect("Addx operand is not an integer. ");
+                Self::Addx(operand)
+            },
+            _ => panic!("Unknown command. "),
+        }
+    }
+
+    pub fn cycles(&self) -> i32 {
+        match self {
+            Self::Noop => 1,
+            Self::Addx(_) => 2, 
+        }
+    }
+
+    pub fn process(&self, input: &i32) -> i32 {
+        match self {
+            Self::Noop => input.clone(),
+            Self::Addx(ref val) => input + val,
+        }
+    }
+}
+
+pub fn value_at_cycle(commands: &Vec<Command>, target_cycle: i32) -> i32 {
+    let mut cycles = 0;
+    let mut x = 1;
+    
+    for com in commands {
+        cycles += com.cycles();
+        if cycles >= target_cycle { break; }
+        x = com.process(&x)
+    }
+    x
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Command, value_at_cycle};
+    use crate::io;
+    use std::path::Path;
+
+    #[test]
+    pub fn day10_example() {
+        let input_vec: Vec<String> = io::read_string_col(Path::new("data/day10/example.txt")).expect("No input found. ");
+        let commands: Vec<Command> = input_vec.iter().map(|string| Command::from_str(string.as_str())).collect();
+        let x: i32 = 1;
+
+        let c20 = value_at_cycle(&commands, 20);
+        assert_eq!(c20 * 20, 420);
+        let c60 = value_at_cycle(&commands, 60);
+        assert_eq!(c60 * 60, 1140);
+        let c100 = value_at_cycle(&commands, 100);
+        assert_eq!(c100 * 100, 1800);
+        let c140 = value_at_cycle(&commands, 140);
+        assert_eq!(c140 * 140, 2940);
+        let c180 = value_at_cycle(&commands, 180);
+        assert_eq!(c180 * 180, 2880);
+        let c220 = value_at_cycle(&commands, 220);
+        assert_eq!(c220 * 220, 3960);
+
+        assert_eq!(c20 * 20 + c60 * 60 + c100 * 100 + c140 * 140 + c180 * 180 + c220 * 220, 13140);
+    }
+
+    #[test]
+    pub fn day10_part1() {
+        let input_vec: Vec<String> = io::read_string_col(Path::new("data/day10/data.txt")).expect("No input found. ");
+        let commands: Vec<Command> = input_vec.iter().map(|string| Command::from_str(string.as_str())).collect();
+        let x: i32 = 1;
+
+        let c20 = value_at_cycle(&commands, 20);
+        let c60 = value_at_cycle(&commands, 60);
+        let c100 = value_at_cycle(&commands, 100);
+        let c140 = value_at_cycle(&commands, 140);
+        let c180 = value_at_cycle(&commands, 180);
+        let c220 = value_at_cycle(&commands, 220);
+
+        assert_eq!(c20 * 20 + c60 * 60 + c100 * 100 + c140 * 140 + c180 * 180 + c220 * 220, 13140);
+    }
+
+}
